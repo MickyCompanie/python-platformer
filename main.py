f0bc5988922d026d0c4e43a0961e51e7889f7498
Micky <micky.companie@gmail.com>
2025-06-30 10:51:38 +0200

    drawing background

--- main.py
+++ main.py
@@ -9,14 +9,33 @@ pygame.init()
 
 pygame.display.set_caption('Python Platformer')
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 800, 600
 FPS = 60
 PLAYER_VEL = 5
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def get_background(name):
+    #important to get file from where the file is
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width+1):
+        for j in range(HEIGHT // height+1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(window, background, bg_image):
+     for tile in background:
+         window.blit(bg_image, tile)
+
+     pygame.display.update()
+
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png")
 
     run = True
     while run:
@@ -26,6 +45,9 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window, background, bg_image)
+
     pygame.quit()
     quit()
 
